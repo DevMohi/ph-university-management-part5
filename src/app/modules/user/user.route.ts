490f55558f374038faf5d5d0e5425ea8463638e9
DevMohi <morphex72@gmail.com>
2025-03-26 13:50:45 +0600

    Comeback

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import { UserControllers } from './user.controller';
 import { createStudentValidationSchema } from '../student/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
+import { createFacultyValidationSchema } from '../faculty/faculty.validation';
 
 const router = express.Router();
 
@@ -10,6 +11,12 @@ router.post(
   '/create-student',
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent,
+);
+
+router.post(
+  '/create-faculty',
+  validateRequest(createFacultyValidationSchema),
+  UserControllers.createFaculty,
 );
 
 //router nijei ekta object
